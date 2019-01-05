61324248145bba99a627e363ceb310a02222af96
Fer Hernandez <fh.1111@gmail.com>
2019-01-05 21:46:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,5 +40,10 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  delete '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    redirect to '/posts'
+  end
 
 end
